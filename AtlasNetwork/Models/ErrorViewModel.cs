c4654074ea9e5dce319a3e0b536cf5b8ac158d8d
ahmettkuurt <65049462+ahmettkuurt@users.noreply.github.com>
2024-06-03 16:03:12 +0300

    Add project files.

---
+++ AtlasNetwork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AtlasNetwork.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
